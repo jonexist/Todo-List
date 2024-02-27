d6ac947d0737750ff9e521d9085227f5ab83342c
Jonexist <79012225+jonexist@users.noreply.github.com>
2024-02-27 14:24:00 +0800

    Fix height value in TodoComponent

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -14,7 +14,7 @@ const TodoComponent = ({ addTask, todos, toggleTodo, deleteTodo }) => {
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-          height: '100vh',
+          height: '100dvh',
           p: '1rem',
         }}
       >
